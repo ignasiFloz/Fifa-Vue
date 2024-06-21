c410902f8d5ad0431669df573dbd09f80fd93436
IgnacioGurrea <gurreaignasi@gmail.com>
2024-06-21 12:41:13 +0200

    structure i18n complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Fifa-Vu';
+  title = 'Fifa-Vue';
+  selectedLanguage = 'es';
+  translateService = Inject(TranslateService);
+ 
+  constructor( ){
+  this.translateService.setDefaultLang('en');
+  this.selectedLanguage = 'en';
+  this.translateService.use(this.selectedLanguage);
+ }
+
+  onLanguageChange() {
+    this.translateService.use(this.selectedLanguage)
+  }
 }
