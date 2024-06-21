534cc39439bbaafba9cbcd4bd5ce09eda85b6fff
ElisendaLopez <elisenda.lv@gmail.com>
2024-06-21 21:10:18 +0200

    models added

--- src/app/models/iplayer.interface.ts
+++ src/app/models/iplayer.interface.ts
@@ -1,2 +1,9 @@
 export interface IPlayer {
+    shotPower: number;
+    penalties: number;
+    longPassing: number;
+    shortPassing: number;
+    dribbling: number;
+    vision: number;
+    ballControl: number;
 }
