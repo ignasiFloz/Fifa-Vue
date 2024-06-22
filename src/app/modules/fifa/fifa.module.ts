2763c05b6b4f0359a07c5540be111a1f22da5438
IgnacioGurrea <gurreaignasi@gmail.com>
2024-06-22 15:21:16 +0200

    service get players

--- src/app/modules/fifa/fifa.module.ts
+++ src/app/modules/fifa/fifa.module.ts
@@ -4,10 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { PlayerDetailsComponent } from './player-details/player-details.component';
 import { PlayerVideosComponent } from './player-videos/player-videos.component';
 import { FifaRoutingModule } from './fifa-routing.module';
+import { FifaService } from '../../services/fifa.service';
 
 
 
 @NgModule({
+  providers: [ 
+    FifaService 
+  ],
   declarations: [
     HomeComponent,
     PlayerDetailsComponent,
