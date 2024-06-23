14aa2d9f1173d7b901ce9f4f3df53a73cf2b3976
ElisendaLopez <elisenda.lv@gmail.com>
2024-06-23 11:38:58 +0200

    json videos url added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'fifa'},
-  {path: 'fifa', loadChildren: () => import('./modules/fifa/fifa.module').then(m => m.FifaModule)}
+  { path: '', pathMatch: 'full', redirectTo: 'fifa' },
+  { path: 'fifa', loadChildren: () => import('./modules/fifa/fifa.module').then(m => m.FifaModule) }
 ];
 
 @NgModule({
