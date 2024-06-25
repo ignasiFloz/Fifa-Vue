8a01995c359ee73529c274d10df9c563a12661ad
IgnacioGurrea <gurreaignasi@gmail.com>
2024-06-25 12:16:36 +0200

    test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, isDevMode } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Fifa-Vue';
- 
+  
+  ngOnInit() {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+  }
 }
