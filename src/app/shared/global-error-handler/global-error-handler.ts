c9bcfb6b2aa51aec59ba4a3d3f32facd611ecb21
ElisendaLopez <elisenda.lv@gmail.com>
2024-06-25 13:17:17 +0200

    folders structure updated

--- src/app/shared/global-error-handler/global-error-handler.ts
+++ src/app/shared/global-error-handler/global-error-handler.ts
@@ -1,7 +1,7 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
-import { NotificationService } from './services/notification.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
